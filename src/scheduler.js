b812c836d52528c3e57be3bc219f4476db3271cb
ambidere <xanthiandere@gmail.com>
2017-01-02 22:47:00 +0800

    added scheduler

--- src/scheduler.js
+++ src/scheduler.js
@@ -6,7 +6,7 @@ var BirthdayFinder = require('./finder');
 var GreetTweet = require(__dirname + '/twitter/greettweet');
 
 var TweetScheduler = function(twitter, data) {
-	this.schedulerInterval = CRONParser.parseExpression('59 23 * * *', {
+	this.schedulerInterval = CRONParser.parseExpression('01 00 * * *', {
 		currentDate: Moment().format('YYYY-MM-DD hh:m:s'),
   		tz: 'Asia/Tokyo'
 	});
